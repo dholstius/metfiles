e77f059e19890adfb412d38aa25700d30b966034
David Holstius <david.holstius@gmail.com>
2011-06-02 11:54:50 -0700

    Clean up SCRAM surface code.

--- R/read.SCRAM.surface.R
+++ R/read.SCRAM.surface.R
@@ -1,4 +1,4 @@
-#' Read surface meteorological data from a file.
+#' Read SCRAM surface meteorological data.
 #'
 #' @param file the file to read
 #' @param \dots further arguments to be passed to \code{read.fwf()}
@@ -8,28 +8,20 @@
 #' @references
 #' \url{http://www.epa.gov/scram001/surface/surfor.txt}
 read.SCRAM.surface <- function(file, ...) {
-	columns <- list(
-		station = 'character',
+	cols <- list(
+		station = 'character',				# NWS station number
 		year = 'integer',
 		month = 'integer',
 		day = 'integer',
 		hour = 'integer',
-		ceiling.height = 'character',
-		wind.directon = 'numeric',
-		wind.speed = 'numeric',
-		dry.bulb.temperature = 'numeric',
-		total.cloud.cover = 'numeric',
-		opaque.cloud.cover = 'numeric')
-	col.names <- c(
-		'station',				# NWS station number
-		'year','month','day','hour',
-		'ceiling.height',			# Hundreds of feet
-		'wind.direction',			# Tens of degrees
-		'wind.speed',				# Knots
-		'dry.bulb.temperatue',		# Degrees Fahrenheit
-		'total.cloud.cover',		# Tens of percent
-		'opaque.cloud.cover')		# Tens of percent
+		ceiling.height = 'character',		# Hundreds of feet
+		wind.directon = 'numeric',			# Tens of degrees
+		wind.speed = 'numeric',				# Knots
+		dry.bulb.temperature = 'numeric',	# Degrees Fahrenheit
+		total.cloud.cover = 'numeric',		# Tens of percent
+		opaque.cloud.cover = 'numeric')		# Tens of percent
 	widths <- c(5, 2, 2, 2, 2, 3, 2, 3, 3, 2, 2)
-	contents <- read.fwf(file, widths, colClasses=columns, col.names=names(columns))
-	transform(contents, ceiling.height=clean.numeric(ceiling.height))
+	raw.contents <- read.fwf(file, widths, colClasses=cols, col.names=names(cols))
+	clean.contents <- transform(contents, ceiling.height=clean.numeric(ceiling.height))
+	return(clean.contents)
 }
