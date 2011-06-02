3969c483ab016d4b563b7fbc2f3bc6beac62a18a
David Holstius <david.holstius@gmail.com>
2011-06-02 11:56:39 -0700

    Relocate package roxygen code

---
+++ R/metfiles-package.R
@@ -0,0 +1,22 @@
+#' Reads meteorology data from formats commonly used in dispersion modeling.
+#'
+#' \tabular{ll}{
+#' Package: \tab metfiles\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.1\cr
+#' Date: \tab 2011-06-02\cr
+#' License: \tab BSD\cr
+#' LazyLoad: \tab yes\cr
+#' 
+#' Supported formats include SCRAM surface (aka MET144), SCRAM upper air, and SAMSON hourly.
+#'
+#' @name metfiles-package
+#' @aliases metfiles
+#' @docType package
+#' @title Reads meteorology data from formats commonly used in dispersion modeling
+#' @author David Holstius
+#' @references
+#' \url{http://www.epa.gov/scram001/surface/surfor.txt}
+#' @keywords package
+#' @seealso \code{\link{read.SCRAM.surface}}
+NULL
